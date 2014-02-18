68ba18e0d4fa75904bb519d47586af3f4284dc6f
Aseem Shakuntal <ashakunt@x201.local>
2014-02-18 14:44:52 +0200

    Dont run the cli if no args given

--- jenkins-cli.sh
+++ jenkins-cli.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-if [ $1 ]; then
-	java -jar jenkins-cli.jar -s http://buildserver:8080/ $1
+server="http://buildserver:8080"
+if [ ! $@ ]; then
+    echo "Please provide an option for the jenkins-cli tool, e.g. help"
+else
+    java -jar jenkins-cli.jar -s $server $@
 fi
