d3c7fb7625b40f3e7581382957318da4e2d5b7ac
Aseem Shakuntal <ashakunt@x201.local>
2014-02-18 16:28:19 +0200

    Offline node checking + bring it online
    Add a groovy script that prints the list of offline nodes
    and a shell script that greps the output and brings the
    node online based on this data

---
+++ node-online.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# date: Feb 18, 2014
+# author: ashakunt <ashakunt@zydus16>
+# 
+# - brings a node online if its not
+# - destined to run only on linux (and not mac at least)
+# requirements: ssh-copy-id -i <identity file> <slaveuser>@<slave>
+
+this_file="$(readlink -f $0)"
+file_location="$(dirname $this_file)"
+export PATH=$file_location:$PATH
+
+# get the slaves we are interested in from the environment
+# variable and also the user. If all the salves have different
+# user, then this script does not handle that at the moment
+SLAVES_TO_CHECK="${SLAVES}"
+SLAVE_USER="${SLAVEUSER}"
+
+jenkins-cli.sh groovy $file_location/offline-nodes.groovy | tee offline-nodes.list
+
+echo "bring-online requested for: $SLAVES_TO_CHECK"
+echo
+
+for SLAVE in $SLAVES_TO_CHECK; do
+    echo "checking: $SLAVE"
+    grep -i $SLAVE offline-nodes.list
+    if [ $? -eq 0 ]; then
+        echo "'$SLAVE' is offline"
+        ssh $SLAVE_USER@$SLAVE ls /home/$SLAVE_USER/jenkins
+        if [ $? -eq 0 ]; then
+            ssh $SLAVE_USER@$SLAVE sh /home/$SLAVE_USER/jenkins/runjenkins.sh
+        else
+            echo "Unable to get ssh hook for $SLAVE"
+        fi
+    else
+        echo "$SLAVE is online!"
+    fi
+done
