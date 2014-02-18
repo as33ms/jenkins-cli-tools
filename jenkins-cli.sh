f8ff3862984eda77804a52082ff62a952e48207c
Aseem Shakuntal <ashakunt@x201.local>
2014-02-18 16:24:50 +0200

    Check for arguments as strings

--- jenkins-cli.sh
+++ jenkins-cli.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 server="http://buildserver:8080"
-if [ ! $@ ]; then
+args="$@"
+if [ "x$args" = "x" ]; then
     echo "Please provide an option for the jenkins-cli tool, e.g. help"
 else
     java -jar jenkins-cli.jar -s $server $@
