4b4ad945519cae90ae539e91185524cef5f43d98
Build Master <master@buildmaster.wumpus.name>
2014-08-22 15:54:46 +0300

    Updated the script for jenkins nightly updates

--- cron.sh
+++ cron.sh
@@ -2,6 +2,13 @@
 blacklist="userContent"
 backupdir=/home/master/jenkins/backups
 
+# locations
+user=buildbot
+pass=dPvU3KQhT7Ck
+server=http://buildserver:8080
+groovy=/home/master/maintenance/status.groovy
+clijar=/home/master/maintenance/jenkins-cli.jar
+
 cd /var/lib/jenkins
 
 dirs=$(echo */)
@@ -19,8 +26,37 @@ do
     done
 done
 
-# create the backup
-tar -czvf $backupdir/${today}.tar.gz $directories
+echo "Directories to backup:"
+echo $directories
+
+java -jar $clijar -s $server groovy $groovy --username $user --password $pass | grep WAIT_MORE -A 10
+waitmore=$?
+
+
+while [ $waitmore -eq 0 ]; do
+    sleep 60
+    java -jar $clijar -s $server groovy $groovy --username $user --password $pass | grep WAIT_MORE -A 10
+    waitmore=$?
+done
+
+if [ $waitmore -ne 0 ]; then
+    echo "Stop jenkins (safe-shutdown)"
+    #java -jar $clijar -s $server safe-shutdown --username $user --password $pass 
+
+    /usr/sbin/service jenkins stop
+
+    echo "Taking backup now"
+
+    mkdir -p $backupdir/$today
+
+    backup=$backupdir/$today/jenkins.tar.gz
+
+    # create the backup
+    tar -czvf $backup $directories
+
+    echo "Putting jenkins up again"
+    /usr/sbin/service jenkins start
+fi
 
 # do some clean up
 # policy: last backup from every month kept forever
